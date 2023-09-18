6eaa9fd23f53382ecd90eb557861d256a183fb75
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2023-09-18 20:08:06 -0300

    adicionado toastcontainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import MainContainer from "@/components/mainContainer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: {session, ...pageProps}, }: AppProps) {
   return (
+    <SessionProvider session={session}>
     <MainContainer>
-      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    <ToastContainer />
+    <Component {...pageProps} />
     </MainContainer>
+    </SessionProvider>
   );
 }
