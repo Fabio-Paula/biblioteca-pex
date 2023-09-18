5705ad1bb10adcb61f9c65306d49b7d33642dc11
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2023-09-18 20:06:15 -0300

    Delete tokenService.tsx

--- src/auth/tokenService.tsx
+++
@@ -1,8 +0,0 @@
-const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN_KEY'
-
-export const tokenService = {
-  save(accessToken : any) {
-    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
-    sessionStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
-  },
-};
