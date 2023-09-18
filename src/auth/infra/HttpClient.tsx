3382836a16567951b04f9d978f430e2f25fc7e66
Fabio Roberto de Paula <fabiopaula.dev@gmail.com>
2023-09-18 20:06:37 -0300

    criado caminho da api e interceptor para salvar dados no session

--- src/auth/infra/HttpClient.tsx
+++ src/auth/infra/HttpClient.tsx
@@ -1,7 +1,25 @@
 import axios from "axios";
+import jwt from "jwt-decode";
+import { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
 
-const baseURL = '192.168.127'
+const baseURL = 'http://localhost:8080'
 
-export const api = axios.create({
-  baseURL
-})
+const ApiClient = (ctx?: GetServerSidePropsContext) => {
+  const defaultOptions = {
+    baseURL,
+  };
+  const instance = axios.create(defaultOptions);
+  instance.interceptors.request.use(async (config) => {
+    const session = await getSession(ctx);
+    if (session) {
+      let decoded = await jwt(String(session.user.access_token))
+      if(((decoded as any).exp * 1000) > Date.now()){
+        config.headers.Authorization = `Bearer ${session.user.access_token}`;
+      }
+    }
+    return config;
+  });
+  return instance;
+};
+export default ApiClient;
